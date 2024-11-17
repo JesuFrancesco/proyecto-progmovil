3f3c69022a834fa04db46bb1aa75cda20c4862bb
JesuFrancesco <117495071+JesuFrancesco@users.noreply.github.com>
2024-11-16 22:49:50 -0500

    chore: otra migracion de columnas...

--- jiron_anime/lib/models/rating_attachment.dart
+++ jiron_anime/lib/models/rating_attachment.dart
@@ -6,15 +6,11 @@ import 'product_rating.dart';
 class RatingAttachment implements ToJson {
   int? ratingId;
   String? imageUrl;
-  String? filename;
-  String? type;
   ProductRating? productRating;
 
   RatingAttachment({
     this.ratingId,
     this.imageUrl,
-    this.filename,
-    this.type,
     this.productRating,
   });
 
@@ -22,8 +18,6 @@ class RatingAttachment implements ToJson {
       RatingAttachment(
           ratingId: json['ratingId'] as int?,
           imageUrl: json['imageUrl'] as String?,
-          filename: json['filename'] as String?,
-          type: json['type'] as String?,
           productRating: json['productRating'] != null
               ? ProductRating.fromJson(
                   json['productRating'] as Map<String, dynamic>)
@@ -32,15 +26,11 @@ class RatingAttachment implements ToJson {
   RatingAttachment copyWith({
     int? ratingId,
     String? imageUrl,
-    String? filename,
-    String? type,
     ProductRating? productRating,
   }) {
     return RatingAttachment(
         ratingId: ratingId ?? this.ratingId,
         imageUrl: imageUrl ?? this.imageUrl,
-        filename: filename ?? this.filename,
-        type: type ?? this.type,
         productRating: productRating ?? this.productRating);
   }
 
@@ -48,8 +38,6 @@ class RatingAttachment implements ToJson {
     return RatingAttachment(
         ratingId: ratingAttachment.ratingId ?? ratingId,
         imageUrl: ratingAttachment.imageUrl ?? imageUrl,
-        filename: ratingAttachment.filename ?? filename,
-        type: ratingAttachment.type ?? type,
         productRating: ratingAttachment.productRating ?? productRating);
   }
 
@@ -57,8 +45,6 @@ class RatingAttachment implements ToJson {
   Map<String, dynamic> toJson() => ({
         if (ratingId != null) 'ratingId': ratingId,
         if (imageUrl != null) 'imageUrl': imageUrl,
-        if (filename != null) 'filename': filename,
-        if (type != null) 'type': type,
         if (productRating != null) 'productRating': productRating
       });
 
@@ -69,15 +55,9 @@ class RatingAttachment implements ToJson {
           runtimeType == other.runtimeType &&
           ratingId == other.ratingId &&
           imageUrl == other.imageUrl &&
-          filename == other.filename &&
-          type == other.type &&
           productRating == other.productRating;
 
   @override
   int get hashCode =>
-      ratingId.hashCode ^
-      imageUrl.hashCode ^
-      filename.hashCode ^
-      type.hashCode ^
-      productRating.hashCode;
+      ratingId.hashCode ^ imageUrl.hashCode ^ productRating.hashCode;
 }
