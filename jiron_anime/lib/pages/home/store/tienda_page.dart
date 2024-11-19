0c030a03000cd13dd3dff8528ea985ea2d0d18e3
JesuFrancesco <117495071+JesuFrancesco@users.noreply.github.com>
2024-11-19 11:31:03 -0500

    fix: fix no hay productos

--- jiron_anime/lib/pages/home/store/tienda_page.dart
+++ jiron_anime/lib/pages/home/store/tienda_page.dart
@@ -175,7 +175,10 @@ class _TiendaPageState extends State<TiendaPage> {
 
   Widget _buildProductoList(List<Tag> tags) {
     if (productoController.productos.isEmpty) {
-      return const Center(child: Text("No hay mangas disponibles"));
+      return Text(
+        "No hay mangas disponibles",
+        style: Theme.of(context).textTheme.titleMedium,
+      );
     }
 
     return GridView.builder(
